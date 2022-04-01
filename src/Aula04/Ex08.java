7a131b363cc238406b20958a69b7ce44796f28df
Davi Taveira <davitaveira@gmail.com>
2022-04-01 20:17:14 -0300

    Final aula estrutura seq

---
+++ src/Aula04/Ex08.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Aula04;
+
+/**
+ *
+ * @author Davi
+ */
+public class Ex08 {
+    
+    public static void main(String[] args) {
+        double minutos,segundos;
+        
+        minutos=120;
+        
+        System.out.println("Segundos: "+minutos*60);
+    }
+    
+}
